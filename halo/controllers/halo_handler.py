9e0ce69c12cf5014f3b89ab0a9054ff34c31788f
Solomon Padilla <goryfigment@gmail.com>
2020-02-19 02:08:14 -0700

    Adding the database to track players for pending leaderboards.

--- halo/controllers/halo_handler.py
+++ halo/controllers/halo_handler.py
@@ -52,12 +52,15 @@ def get_xbox_auth():
                          },
                          verify=False
     )
-    ppft = re.search(R_PPFT, response_one.text).group(1)
-    ppsx = re.search(R_PPSX, response_one.text).group(1)
+
     try:
+        ppft = re.search(R_PPFT, response_one.text).group(1)
+        ppsx = re.search(R_PPSX, response_one.text).group(1)
         post = re.search(R_URLPOST, response_one.text).group(1)
     except AttributeError:
         response_one = s.get(oauth20_authorize, headers={'user-agent': USER_AGENT, 'host': 'login.live.com'}, verify=False)
+        ppft = re.search(R_PPFT, response_one.text).group(1)
+        ppsx = re.search(R_PPSX, response_one.text).group(1)
         post = re.search(R_URLPOST, response_one.text).group(1)
 
     response_two = s.post(post,
