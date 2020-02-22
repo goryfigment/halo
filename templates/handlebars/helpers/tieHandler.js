6164bf24c787f7bdcaa35999729965201c22de0d
Solomon Padilla <goryfigment@gmail.com>
2020-02-22 03:54:37 -0700

    fixed leaderboards error

--- templates/handlebars/helpers/tieHandler.js
+++ templates/handlebars/helpers/tieHandler.js
@@ -2,7 +2,8 @@ module.exports = function(x) {
     if (!(x in globals.tie) && globals.page == 1){
         globals.rank++;
         globals.tie[x] = globals.rank;
+        return globals.rank;
+    } else {
+        return 4;
     }
-
-    return globals.rank;
 };
