4a43944ffecdeb93a0a4f23afe13b7bff7a5c033
omni-frontend-dev <spadilla@omnislash.com>
2023-09-28 17:58:18 -0700

    fix date conversion

--- templates/handlebars/helpers/hoursToDay.js
+++ templates/handlebars/helpers/hoursToDay.js
@@ -1,6 +1,6 @@
 module.exports = function(date) {
   if(date.includes('h')) {
-  	var number = date.split('h')[0]
+  	var number = date.split('h')[0].replace(',', '')
     var days = (Math.floor(Number(number) / 24)).toString()
     var hours = (Number(number) % 24)
     return days + 'd ' + date.replace(number, hours)
